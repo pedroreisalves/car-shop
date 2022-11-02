c4fe914d34c5f1044b10bd0ee312125b130e29fd
Pedro Reis <pedronr0309@gmail.com>
2022-11-02 08:54:03 -0300

    feat: create mocks

---
+++ src/tests/mocks/motorcycleMock.ts
@@ -0,0 +1,20 @@
+import { IMotorcycle } from './../../interfaces/IMotorcycle';
+
+export const motorcycleMock: IMotorcycle = {
+  model: "Honda CG Titan 125",
+  year: 1963,
+  color: "red",
+  buyValue: 3500,
+  category: "Street",
+  engineCapacity: 125
+};
+
+export const motorcycleMockWithId: IMotorcycle & { _id: string } = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Honda CG Titan 125",
+  year: 1963,
+  color: "red",
+  buyValue: 3500,
+  category: "Street",
+  engineCapacity: 125
+};
